37f217beffb00a089d49c3e688714be135790c3d
Patrick Bomm <p.bomm@edu.pucrs.br>
2022-11-15 20:21:05 -0300

    Modify the code and add the tests

--- src/Graph.py
+++ src/Graph.py
@@ -1,25 +1,29 @@
 from tkinter import *
 import webbrowser
+from graphviz import Digraph
 
 
 class Graph:
 
+    # Constructor
     def __init__(self, nodes: list, directed: bool = True):
         self.nodes = nodes
         self.directed = directed
         self.listAdj = {node: list() for node in nodes}
 
+    # Add a edge to 2 nodes
     def addedge(self, node1: str, node2: str):
         self.listAdj[node1].append(node2)
 
         if not self.directed:
             self.listAdj[node2].append(node1)
-
+    
     def printNodesAndEdges(self):
-        for key in self.listAdj.keys():
-            print(f"Node {key}: {self.listAdj[key]}")
+        for i in self.listAdj.keys():
+            print(f"Node {i}: {self.listAdj[i]}")
 
-    def canAcess(self, value1: str, value2: str) -> bool:
+    # Verify if can be connected from node1 to node2
+    def canAccess(self, value1: str, value2: str) -> bool:
 
         alreadyVisited = []
         queue = []
@@ -40,29 +44,40 @@ class Graph:
 
         return False
 
+    # Count how many items can have two flavors
     def pathUpto2Interactions(self) -> int:
         counter = 0
 
         for i in self.listAdj.keys():
             for j in self.listAdj.keys():
                 if j != i:
-                    if self.canAcess(i, j):
+                    if self.canAccess(i, j):
                         counter = counter + 1
 
         return counter
 
+    # Count how many items can have three flavor
     def pathUpto3Interactions(self) -> int:
         counter = 0
         for i in self.listAdj.keys():
             for j in self.listAdj.keys():
                 if j != i:
-                    if self.canAcess(i, j):
+                    if self.canAccess(i, j):
                         for nextJ in self.listAdj.keys():
                             if nextJ not in [i, j]:
-                                if self.canAcess(j, nextJ):
+                                if self.canAccess(j, nextJ):
                                     counter = counter + 1
 
         return counter
+    
+    def generateDot(self):
+        dot = Digraph(comment='Graph', format='png')
+        for key in self.listAdj.keys():
+            for value in self.listAdj[key]:
+                dot.edge(key, value)
+        dot.render('assets/graph/graphDot.txt', view=True)
+
+# Read the text file and add the nodes to the list
 
 
 def readTxt(file: str) -> Graph:
@@ -91,15 +106,11 @@ def readTxt(file: str) -> Graph:
 
 
 class Application(Frame):
-
-
+    
     def __init__(self, master=None):
-        # Read the configuration file
-        graph = readTxt('assets/test.txt')
-
-        # Create the interface    
         self.fontePadrao = ("Roboto", "14")
         self.fonteSecundaria = ("Arial", "12")
+
         self.primeiroContainer = Frame(master)
         self.primeiroContainer["pady"] = 10
         self.primeiroContainer.pack()
@@ -124,25 +135,39 @@ class Application(Frame):
         self.titulo["font"] = ("Roboto", "15", "bold")
         self.titulo.pack()
 
-        text = "Number of variations up to 3 flavors: " + (str(graph.pathUpto3Interactions()))
+    
+        # If want change the file, change the name here
+        g = readTxt("assets/test20.txt")
 
-        self.path3flavors = Label(self.terceiroContainer,
-                                  text=text, font=self.fontePadrao)
-        self.path3flavors.pack(side=LEFT)
+        # Generate the graph to put on graphviz online
+        try:
+            g.generateDot()
+        except:
+            print("")
+        
+        if g != None:
 
-        text2 = "Number of variations up to 2 flavors: " + (str(graph.pathUpto2Interactions()))
+            text = "Number of variations up to 3 flavors: " + \
+                (str(g.pathUpto3Interactions()))
 
-        self.path2flavors = Label(self.segundoContainer,
-                                  text=text2, font=self.fontePadrao)
-        self.path2flavors.pack(side=LEFT)
+            self.path3flavors = Label(self.terceiroContainer,
+                                      text=text, font=self.fontePadrao)
+            self.path3flavors.pack(side=LEFT)
 
-        self.openSite = Button(self.quintoContainer, text="Site to see the Graph",
-                               font=self.fontePadrao, command=self.openSite)
-        self.openSite.pack(side=RIGHT)
+            text2 = "Number of variations up to 2 flavors: " + \
+                (str(g.pathUpto2Interactions()))
 
-        self.text = Label(
-            self.quartoContainer, text="Copy the text at 'grafo.dot' file to paste at site!", font=self.fonteSecundaria)
-        self.text.pack(side=LEFT)
+            self.path2flavors = Label(self.segundoContainer,
+                                      text=text2, font=self.fontePadrao)
+            self.path2flavors.pack(side=LEFT)
+
+            self.openSite = Button(self.quintoContainer, text="Site to see the Graph",
+                                   font=self.fontePadrao, command=self.openSite)
+            self.openSite.pack(side=RIGHT)
+
+            self.text = Label(
+                self.quartoContainer, text="Copy the text at 'graphDot.txt' file to paste at site!", font=self.fonteSecundaria)
+            self.text.pack(side=LEFT)
 
     def openSite(self):
         webbrowser.open("https://dreampuf.github.io/GraphvizOnline/#")
